8350c81f8b73e80f41012e834dd2e7f5a6e1bbe7
Snowblaze <a.a.matinyan@gmail.com>
2020-08-11 18:37:14 +0400

    Fix quaternion concatenation

--- Assets/Scripts/MathExtensions.cs
+++ Assets/Scripts/MathExtensions.cs
@@ -10,9 +10,9 @@ public static class MathExtensions
     public static quaternion Concatenate(quaternion lhs, quaternion rhs)
     {
         float3 lhsXYZ = lhs.value.xyz;
-        float3 rhsXYZ = lhs.value.xyz;
+        float3 rhsXYZ = rhs.value.xyz;
         float lhsW = lhs.value.w;
-        float rhsW = lhs.value.w;
+        float rhsW = rhs.value.w;
 
         float3 axis = lhsW * rhsXYZ + rhsW * lhsXYZ + math.cross(lhsXYZ, rhsXYZ);
         float scalar = lhsW * rhsW - math.dot(lhsXYZ, rhsXYZ);
